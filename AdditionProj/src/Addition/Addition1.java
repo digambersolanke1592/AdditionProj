2864789838042ef79533780399f91b1deb83d064
digambersolanke1592 <105357495+digambersolanke1592@users.noreply.github.com>
2022-10-14 15:26:48 +0530

    third commit

--- AdditionProj/src/Addition/Addition1.java
+++ AdditionProj/src/Addition/Addition1.java
@@ -3,6 +3,15 @@ package Addition;
 public class Addition1 {
 	public static void main(String[] args) {
 		
+		
+		int a = 10;
+		int b = 20 ;
+		
+		int sum ;
+		
+		sum = a + b ;
+		
+		System.out.println(sum);
 	}
 
 }
